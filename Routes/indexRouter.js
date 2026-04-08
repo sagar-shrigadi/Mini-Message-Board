efc5c77dec1fbfe46d48c1c0b1d62f9c86e42676
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-04-08 20:08:42 +0530

    feat: add ejs template files & update root route to send dynamic html contents
    - add header.ejs & footer.ejs templates
    - add index.ejs for root page
    - include header.ejs and footer.ejs in index.ejs
    - update the root route in indexRouter.js to render html using templates
    - add prettierignore to exclude ejs files

--- Routes/indexRouter.js
+++ Routes/indexRouter.js
@@ -2,6 +2,24 @@ import express from "express";
 
 export const indexRouter = express.Router();
 
+export const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.send("from router, Hello Mini Msg Board!");
+  res.render("pages/index", {
+    title: "Mini Message Board",
+    newPath: "/new",
+    btnMsg: "Add Message",
+    messages,
+  });
 });
